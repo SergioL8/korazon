4a35d06f233c6179e178bea8dd6fc42ce6722ea7
Mario Castro <macasmu04@gmail.com>
2025-06-15 18:43:03 +0200

    FilterChip widget

---
+++ lib/src/widgets/filterChip.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class FilterChipLabel extends StatelessWidget {
+  final String label;
+  final bool selected;
+  final VoidCallback onTap;
+
+  const FilterChipLabel({
+    Key? key,
+    required this.label,
+    required this.selected,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: SizedBox(
+          width: 100, // 👈 Set a fixed width (adjust as needed)
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            decoration: BoxDecoration(
+              color: selected ? Colors.white : Colors.transparent,
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text(
+              label,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: selected ? Colors.black : Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
